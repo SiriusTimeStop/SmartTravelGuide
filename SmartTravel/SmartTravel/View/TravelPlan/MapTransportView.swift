61f2dcab99ef637f883103001830d58828df95c3
SiriusTimeStop <jackychoikalok@gmail.com>
2024-02-13 19:50:34 +0800

    Map routes

--- SmartTravel/SmartTravel/View/TravelPlan/MapTransportView.swift
+++ SmartTravel/SmartTravel/View/TravelPlan/MapTransportView.swift
@@ -11,12 +11,50 @@ import MapKit
 
 struct MapTransportView: View {
     let post: Post
+    
+    //2.
+    @State private var DirectMethod: [String] = []
+    @State private var DirectMethod2: [String] = []
     var body: some View {
         VStack{
-            NewMapView(post: post)
+            NewMapView(post: post,DirectMethod: $DirectMethod,DirectMethod2: $DirectMethod2)
                 .frame(width: .infinity,height: 400)
+            
+            VStack{
+                Text("Directions")
+                    .font(.largeTitle)
+                    .bold()
+                    .padding()
+                
+                Divider().background(Color.blue)
+                
+                List{
+                    Text(post.location1)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                    ForEach(0..<self.DirectMethod.count,id: \.self){i in
+                        Text(self.DirectMethod[i])
+                            .padding()
+                    }
+                    Text(post.location2)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                    ForEach(0..<self.DirectMethod2.count,id: \.self){i in
+                        Text(self.DirectMethod2[i])
+                            .padding()
+                    }
+                    Text(post.location3)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                }
+            }
+            .padding(.bottom,130)
         }
         .vAlign(.top)
+        .ignoresSafeArea()
     }
 }
 
@@ -24,6 +62,9 @@ struct MapTransportView: View {
 struct NewMapView : UIViewRepresentable{
     let post: Post
     
+    //1.
+    @Binding var DirectMethod: [String]
+    @Binding var DirectMethod2: [String]
     
     func makeCoordinator() -> Coordinator {
         return NewMapView.Coordinator()
@@ -61,7 +102,7 @@ struct NewMapView : UIViewRepresentable{
         let req = MKDirections.Request()
         req.source = MKMapItem(placemark: MKPlacemark(coordinate: sourceCoordinate))
         req.destination = MKMapItem(placemark: MKPlacemark(coordinate: destinationCoordinate))
-        
+        req.transportType = .automobile
         let directions = MKDirections(request: req)
         
         directions.calculate{ (direct,err) in
@@ -69,15 +110,18 @@ struct NewMapView : UIViewRepresentable{
                 print((err?.localizedDescription)!)
                 return
             }
-            
+            guard let route = direct?.routes.first else{ return }
             let polyline = direct?.routes.first?.polyline
             map.addOverlay(polyline!)
 //            map.setRegion(MKCoordinateRegion(polyline!.boundingMapRect), animated: true)
+            //3.
+            self.DirectMethod = route.steps.map{$0.instructions}.filter{!$0.isEmpty}
         }
         
         let req2 = MKDirections.Request()
         req2.source = MKMapItem(placemark: MKPlacemark(coordinate: destinationCoordinate))
         req2.destination = MKMapItem(placemark: MKPlacemark(coordinate: thirdCoordinate))
+        req2.transportType = .automobile
         
         let directions2 = MKDirections(request: req2)
         
@@ -86,10 +130,13 @@ struct NewMapView : UIViewRepresentable{
                 print((err?.localizedDescription)!)
                 return
             }
-            
+            guard let route2 = direct?.routes.first else{ return }
             let polyline2 = direct?.routes.first?.polyline
             map.addOverlay(polyline2!)
             map.setRegion(MKCoordinateRegion(polyline2!.boundingMapRect), animated: true)
+            map.setVisibleMapRect(polyline2!.boundingMapRect, edgePadding: UIEdgeInsets(top: 50, left: 50, bottom: 50, right: 50), animated: true)
+            //3.
+            self.DirectMethod2 = route2.steps.map{$0.instructions}.filter{!$0.isEmpty}
         }
         return map
     }
